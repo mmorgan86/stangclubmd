f91e8c8cb90729a19402815454174ebb39b06275
mmorgan86 <michael1986morgan@gmail.com>
2019-03-18 22:58:29 -0400

    updated errors and sanitize user input

--- register.php
+++ register.php
@@ -1,4 +1,5 @@
 <?php
+session_start();
 require_once "db.php";
 
 // Declaring variables to prevent errors
@@ -16,21 +17,25 @@ if (isset($_POST['register_button'])) {
     $fname = mysqli_real_escape_string($conn, $_POST['reg_fname']); // remove html tags
     $fname = str_replace(' ', '', $fname); // remove spaces
     $fname = ucfirst(strtolower($fname)); // uppercase first letter
+    $_SESSION['reg_fname'] = $fname; // stores first name into session variable
 
     // last name
     $lname = mysqli_real_escape_string($conn, $_POST['reg_lname']); // remove html tags
     $lname = str_replace(' ', '', $lname); // remove spaces
     $lname = ucfirst(strtolower($lname)); // uppercase first letter
+    $_SESSION['reg_lname'] = $lname; // stores last name into session variable
 
     // email
     $em = mysqli_real_escape_string($conn, $_POST['reg_email']); // remove html tags
     $em = str_replace(' ', '', $em); // remove spaces
     $em = ucfirst(strtolower($em)); // uppercase first letter
+    $_SESSION['reg_email'] = $em; // stores email into session variable
 
     // email2
     $em2 = mysqli_real_escape_string($conn, $_POST['reg_email2']); // remove html tags
     $em2 = str_replace(' ', '', $em2); // remove spaces
     $em2 = ucfirst(strtolower($em2)); // uppercase first letter
+    $_SESSION['reg_email2'] = $em2; // stores email2 into session variable
 
     // password
     $password = mysqli_real_escape_string($conn, $_POST['reg_password']); // remove html tags
@@ -54,31 +59,32 @@ if (isset($_POST['register_button'])) {
             $num_rows = mysqli_num_rows($e_check);
 
             if ($num_rows > 0) {
-                echo "Email already in use";
+                array_push($error_array, "Email already in use<br>");
             }
         } else {
-            echo "Invalid format";
+            array_push($error_array, "Invalid format <br>");
         }
     } else {
-        echo "Emails don't match";
+        array_push($error_array, "Emails don't match<br>");
     }
 
     if (strlen($fname) > 25 || strlen($fname) < 2) {
-        echo "Your first name must be between 2 and 25 characters";
+        array_push($error_array, "Your first name must be between 2 and 25 characters<br>");
     }
     if (strlen($lname) > 25 || strlen($lname) < 2) {
-        echo "Your last name must be between 2 and 25 characters";
+        array_push($error_array, "Your last name must be between 2 and 25 characters<br>");
     }
     if ($password != $password2) {
-        echo "Your passwords do not match!";
+        array_push($error_array, "Your passwords do not match!<br>");
     } else {
         if (preg_match('/[^A-Za-z0-9]/', $password)) {
-            echo "Your password can only contain english characters or numbers";
+            array_push($error_array, "Your password can only contain english characters or numbers<br>");
         }
     }
     if (strlen($password) > 30 || strlen($password) < 5) {
-        echo "Your password must be between 5 and 30 characters";
+        array_push($error_array, "Your password must be between 5 and 30 characters<br>");
     }
+
 }
 ?>
 
@@ -94,13 +100,56 @@ if (isset($_POST['register_button'])) {
 </head>
 <body>
 
-<form action="register.php" method="POST">
-  <input type="text" name="reg_fname" placeholder="First Name" required><br>
-  <input type="text" name="reg_lname" placeholder="Last Name" required><br>
-  <input type="email" name="reg_email" placeholder="Email" required><br>
-  <input type="email" name="reg_email2" placeholder="Confirm Email" required><br>
+<form class="text-center" action="register.php" method="POST">
+  <input type="text" name="reg_fname" placeholder="First Name"
+    value="<?php if (isset($_SESSION['reg_fname'])) {
+    echo $_SESSION['reg_fname'];
+}?>"
+  required><br>
+  <?php if (in_array("Your first name must be between 2 and 25 characters<br>", $error_array)) {
+    echo "<small class='text-danger'>Your first name must be between 2 and 25 characters</small><br>";
+}
+?>
+
+  <input type="text" name="reg_lname" placeholder="Last Name"
+    value="<?php if (isset($_SESSION['reg_lname'])) {
+    echo $_SESSION['reg_lname'];
+}?>"
+  required><br>
+  <?php if (in_array("Your last name must be between 2 and 25 characters<br>", $error_array)) {
+    echo "<small class='text-danger'>Your last name must be between 2 and 25 characters</small><br>";
+}
+?>
+
+  <input type="email" name="reg_email" placeholder="Email"
+  value="<?php if (isset($_SESSION['reg_email'])) {
+    echo $_SESSION['reg_email'];
+}?>"
+required><br>
+
+  <input type="email" name="reg_email2" placeholder="Confirm Email"
+  value="<?php if (isset($_SESSION['reg_email2'])) {
+    echo $_SESSION['reg_email2'];
+}?>"required><br>
+<?php if (in_array("Email already in use<br>", $error_array)) {
+    echo "<small class='text-danger'>Email already in use</small><br>";
+} else if (in_array("Invalid format <br>", $error_array)) {
+    echo "<small class='text-danger'>Invalid format</small><br>";
+} else if (in_array("Emails don't match<br>", $error_array)) {
+    echo "<small class='text-danger'>Emails don't match</small><br>";
+}
+?>
+
   <input type="password" name="reg_password" placeholder="Password" required><br>
   <input type="password" name="reg_password2" placeholder="Confirm Password" required><br>
+  <?php if (in_array("Your passwords do not match!<br>", $error_array)) {
+    echo "<small class='text-danger'>Your passwords do not match!</small><br>";
+} else if (in_array("Your password can only contain english characters or numbers<br>", $error_array)) {
+    echo "<small class='text-danger'>Your password can only contain english characters or numbers</small><br>";
+} else if (in_array("Your password must be between 5 and 30 characters<br>", $error_array)) {
+    echo "<small class='text-danger'>Your password must be between 5 and 30 characters</small><br>";
+}
+?>
   <input type="submit" name="register_button" value="Register">
 </form>
 
