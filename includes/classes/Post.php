4b147724039a79f8ef3b97082fc5a487e746259d
mmorgan86 <michael1986morgan@gmail.com>
2019-04-20 02:33:13 -0400

    added functionality to load post of profile, finished delete post

--- includes/classes/Post.php
+++ includes/classes/Post.php
@@ -101,7 +101,7 @@ class Post {
 
           // delete post button
           if($userLoggedIn == $added_by) {
-            $delete_button = '<button class="delete_button btn-danger" id="post'.$id.'">X</button>';
+            $delete_button = '<button class="delete_button btn-danger" id="post'.$id.'">x</button>';
           } else {
             $delete_button = '';
           }
@@ -233,7 +233,7 @@ class Post {
           $(document).ready(function() {
             $('#post<?php echo $id; ?>').on('click', function () {
               bootbox.confirm('Are you sure you want to delete this post?', function (result) {
-                $.post("includes/formHandlers/delete_post.php?post_id<?php echo $id; ?>", 
+                $.post("includes/formHandlers/delete_post.php?post_id=<?php echo $id; ?>", 
                 {result:result});
                 if(result) {
                   setTimeout(function(){
@@ -246,6 +246,202 @@ class Post {
         </script>
         <?php
         }
+      } // End while loop
+      if($count > $limit) {
+        $str .= "<input type='hidden' class='nextPage' value='".($page + 1)."'>
+                  <input type='hidden' class='noMorePosts' value='false'>";
+      } else {
+        $str .= "<input type='hidden' class='noMorePosts' value='true'><p style='text-align:center;'> No more posts to show </p>";
+      }
+    }
+    echo $str;
+  }
+
+  public function loadProfilePost($data, $limit) {
+
+    $page = $data['page'];
+    $userLoggedIn = $this->user_obj->getUsername();
+    $profileUser = $data['profileUser'];
+
+    if($page == 1) {
+      $start = 0;
+    } else {
+      $start = ($page - 1) * $limit;
+    }
+
+    $str = ''; // string to return
+    $data_query = mysqli_query($this->conn, "SELECT * FROM posts WHERE deleted ='no' AND ((added_by='$profileUser' AND user_to='none') OR user_to='$profileUser') ORDER BY id DESC");
+
+    if(mysqli_num_rows($data_query) > 0) {
+       
+      // count number of results 
+      $num_iterations = 0; 
+      $count = 1;
+
+
+      while($row = mysqli_fetch_array($data_query)) {
+        $id = $row['id'];
+        $body = $row['body'];
+        $added_by = $row['added_by'];
+        $date_time = $row['date_added'];
+          
+          // get to number of post that have been loaded
+          if($num_iterations++ < $start)
+            continue;
+
+          // once 10 posts have been loaded break
+          if($count > $limit) {
+            break;
+          } else {
+            $count++;
+          }
+
+          // delete post button
+          if($userLoggedIn == $added_by) {
+            $delete_button = '<button class="delete_button btn-danger" id="post'.$id.'">x</button>';
+          } else {
+            $delete_button = '';
+          }
+
+          // get user details
+          $user_details_query = mysqli_query($this->conn, "SELECT first_name, last_name, profile_pic, username FROM users WHERE username = '$added_by'");
+          $user_row = mysqli_fetch_array($user_details_query);
+
+          // Get profile pic
+          $first_name = $user_row['first_name'];
+          $last_name = $user_row['last_name'];
+          $profile_pic = $user_row['profile_pic'];
+          $username = $user_row['username'];
+          
+          ?>
+          <!-- Dispaly comments -->
+          <script>
+            // toggle button to show and hide comments section for each post
+            function toggle<?php echo $id; ?>() {
+
+              // if a tag (<a>) is clicked don't show comments
+              var target = $(event.target);
+              if (!target.is("a") && !target.is("button")) {
+
+                var el = document.getElementById('toggleComment<?php echo $id; ?>');
+
+                if(el.style.display == 'block') {
+                  el.style.display = 'none';
+                }else {
+                  el.style.display = 'block';
+                }
+              }
+            }
+          </script>
+
+          <?php
+
+          $comments_check = mysqli_query($this->conn, "SELECT * FROM comments WHERE post_id = '$id'");
+          $comments_check_num = mysqli_num_rows($comments_check);
+          // Get timeframe
+          $date_time_now = date("Y-m-d H:i:s");
+          // time of post
+          $start_date = new DateTime($date_time);
+          // current time
+          $end_date = new DateTime($date_time_now); 
+
+          // difference between dates
+          $interval = $start_date->diff($end_date);
+
+          if($interval->y >= 1) {
+            if($interval->y == 1) {
+              // 1 year ago
+              $time_message = $interval->y ." year ago"; // 1 year ago
+            } else {
+              $time_message = $interval->y. " years ago"; // 1+ year ago
+            }
+          } else if($interval->m >= 1) {
+              if($interval->d == 0) {
+              $days = " ago";
+              }
+              else if ($interval->d == 1){
+                $days = $interval->d . " day ago";
+              }
+              else {
+                $days = $interval->d . " days ago";
+              }
+              if($interval->m == 1) {
+                $time_message = $interval->m. " month" .$days;
+              }
+              else {
+                $time_message = $interval->m. " months" .$days;
+              }
+          } else if($interval->d >= 1) {
+              if($interval->d == 1) {
+                $time_message = ' Yesterday';
+              } else {
+                $time_message = $interval->d ." days ago";
+              }
+          } else if($interval->h >= 1) {
+              if($interval->h == 1) {
+                $time_message = $interval->h .' hour ago';
+              } else {
+                $time_message = $interval->h ." hours ago";
+              }
+            
+          }else if($interval->i >= 1) {
+              if($interval->i == 1) {
+                $time_message = $interval->i .' minute ago';
+              } else {
+                $time_message = $interval->i." mins ago";
+              }
+
+          }
+          else {
+            if($interval->s < 30) {
+              $time_message = ' Just now';
+            } else {
+              $time_message = $interval->s ." seconds ago";
+            }
+          }
+          $str .= "<div class='status_post' onClick='javascript:toggle$id()'>
+                    <div class='post_profile_pic'>
+                      <img src='$profile_pic' width='50'>
+                    </div>
+                    <div class='post_by' style='color:#ACACAC;'>
+                      <a href='$added_by'>$username </a> $user_to &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;$time_message
+                      $delete_button
+                    </div>
+                    <div id='post_body'>
+                      $body
+                      <br>
+                      <br>
+                      <br>
+                    </div>
+
+                    <div class='newsFeedPostOptions'>
+                      Comments($comments_check_num)&nbsp;&nbsp;&nbsp;
+                      <iframe src='like.php?post_id=$id' scrolling='no'></iframe>
+                    </div>
+                  </div>
+                
+                  <div class='post_comment' id='toggleComment$id' style='display:none;'>
+                    <iframe src='comment_frame.php?post_id=$id' id='comment_iframe' frameborder='0'></iframe>
+                  </div>
+                  <hr>";
+
+        ?>
+        <script>
+          $(document).ready(function() {
+            $('#post<?php echo $id; ?>').on('click', function () {
+              bootbox.confirm('Are you sure you want to delete this post?', function (result) {
+                $.post("includes/formHandlers/delete_post.php?post_id=<?php echo $id; ?>", 
+                {result:result});
+                if(result) {
+                  setTimeout(function(){
+                    location.reload();
+                  }, 300);
+                }
+              });
+            });
+          });
+        </script>
+        <?php
       } // End while loop
       if($count > $limit) {
         $str .= "<input type='hidden' class='nextPage' value='".($page + 1)."'>
--- profile.php
+++ profile.php
@@ -109,6 +109,101 @@ if (isset($_POST['respond_request'])) {
   </div>
 </div>
 
+  <script>
+  //  dynamically load post on page scroll
+  $(function(){
+    
+      var userLoggedIn = '<?php echo $userLoggedIn; ?>';
+      var profileUsername = '<?php echo $username ?>';
+      var inProgress = false;
+
+      loadPosts(); //Load first posts
+
+      $(window).scroll(function() {
+          var bottomElement = $(".status_post").last();
+          var noMorePosts = $('.post_area').find('.noMorePosts').val();
+
+          // isElementInViewport uses getBoundingClientRect(), which requires the HTML DOM object, not the jQuery object. The jQuery equivalent is using [0] as shown below.
+          if (isElementInView(bottomElement[0]) && noMorePosts == 'false') {
+              loadPosts();
+          }
+      });
+
+      function loadPosts() {
+          if(inProgress) { //If it is already in the process of loading some posts, just return
+              return;
+          }
+        
+          inProgress = true;
+          $('#loading').show();
+
+          var page = $('.post_area').find('.nextPage').val() || 1; //If .nextPage couldn't be found, it must not be on the page yet (it must be the first time loading posts), so use the value '1'
+
+          $.ajax({
+              url: "includes/handlers/ajax_load_profile_posts.php",
+              type: "POST",
+              data: "page=1&userLoggedIn=" + userLoggedIn + "&profileUsername=" + profileUsername,
+              cache:false,
+
+              success: function(response) {
+                  $('.post_area').find('.nextPage').remove(); //Removes current .nextpage
+                  $('.post_area').find('.noMorePosts').remove(); //Removes current .nextpage
+                  $('.post_area').find('.noMorePostsText').remove(); //Removes current .nextpage
+
+                  $('#loading').hide();
+                  $(".post_area").append(response);
+
+                  inProgress = false;
+              }
+          });
+       }
+
+      $(window).scroll(function() {
+        var height = $('.post_area').height(); // Div containing posts
+        var scroll_top = $(this).scrollTop();
+        var page = $('.post_area').find('.nextPage').val();
+        var noMorePosts = $('.post_area').find('.noMorePosts').val();
+
+        if((document.body.scrollHeight == document.body.scrollTop + window.innerHeight) && noMorePosts == 'false') {
+          $('#loading').show();
+
+          var ajaxReg = $.ajax({
+            url: "includes/handlers/ajax_load_profile_posts.php",
+            type: "POST"
+            data: "page=" + page = "&userLoggedIn=" + userLoggedIn + "&profileUsername=" + profileUsername,
+            cache: false,
+
+            success: function(response) {
+              $('.post_area').find('.nextPage').remove();
+              $('.post_area').find('.noMorePosts').remove();
+
+              $('#loading').hide();
+              $('.posts_area').append(response);
+            }
+          });
+        } // END IF
+        return false;
+      });
+ 
+      //  //Check if the element is in view
+      //   function isElementInView (el) {
+      //     if(el == null) {
+      //       return;
+      //     }
+  
+      //     var rect = el.getBoundingClientRect();
+  
+      //     return (
+      //       rect.top >= 0 &&
+      //       rect.left >= 0 &&
+      //       rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) && //* or $(window).height()
+      //       rect.right <= (window.innerWidth || document.documentElement.clientWidth) //* or $(window).width()
+      //     );
+      //   }
+      });
+ 
+   </script>
+
 
 </div> <!-- End of wrapper (in header.php) -->
 </body>
